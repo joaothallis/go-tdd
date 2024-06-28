dd1ad1af7e14095796a94233d9e060ac13642f5a
João Thallis <joaothallis@icloud.com>
2024-06-28 17:50:55 -0300

    Add IfContainsReturnString

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,18 @@
 package iteration
 
+import "strings"
+
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
 	return repeated
+}
+
+func IfContainsReturnString(s string, substr string) string {
+	if strings.Contains(s, substr) {
+		return s
+	}
+	return ""
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -5,6 +5,23 @@ import (
 	"testing"
 )
 
+func TestIfContainsReturnString(t *testing.T) {
+	t.Run("not contains", func(t *testing.T) {
+		got := IfContainsReturnString("a", "b")
+		want := ""
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("contains", func(t *testing.T) {
+		got := IfContainsReturnString("a", "a")
+		want := "a"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 6)
 	expected := "aaaaaa"
