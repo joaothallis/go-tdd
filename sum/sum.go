c54e4fa091b5f09db5d593ba8d8ebc336c12ba05
João Thallis <joaothallis@icloud.com>
2024-06-29 21:47:09 -0300

    Enough code to pass Sum

--- sum/sum.go
+++ sum/sum.go
@@ -1,9 +1,9 @@
 package main
 
-func Sum(numbers []int) int {
+func Sum(numbers [5]int) int {
 	sum := 0
-	for _, number := range numbers {
-		sum += number
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
 	}
 	return sum
 }
